7fa3b9acbe90330c29d7b0ef9c0ce81efb22c72e
Jezerig <jesse_pasanen@hotmail.com>
2022-04-07 07:14:50 +0300

    Init

---
+++ BFS_wikipedia.py
@@ -0,0 +1,13 @@
+# BFS_wikipedia.py
+# Jesse Pasanen 0545937
+# Distributed Systems Final Project
+# Sources:
+#
+
+def main():
+    print("Init.")
+
+if __name__ == '__main__':
+    main()
+
+# EOF
